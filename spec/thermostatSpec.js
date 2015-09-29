545dccbe80a53ad5f4f6151c96ee656946b355ab
Tim Hyson <tjhyson@gmail.com>
2015-09-29 16:38:09 +0100

    temperature can be increased

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,8 +1,8 @@
 describe('Thermostat', function() {
 
-  var thermostat
+  var thermostat;
 
-  beforeEach(function () {
+  beforeEach(function() {
     thermostat = new Thermostat();
   });
 
@@ -11,6 +11,12 @@ describe('Thermostat', function() {
     it('starts at 20 degrees', function() {
       expect(thermostat.temperature).toEqual(20);
     });
+
+    it('can be increased', function() {
+      thermostat.temperatureRaise()
+      expect(thermostat.temperature).toEqual(21);
+    });
+
 
   });
 
